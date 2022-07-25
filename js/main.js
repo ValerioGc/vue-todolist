52675f200ec93fa6d31aae09114c149b23059301
ValerioGc <valerio.gc1@gmail.com>
2022-07-25 15:26:52 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var container = new Vue({
+    el:'#container',
+    data: {
+        arrList: [
+            {
+                text: 'ripassare javscript',
+                done: false
+            },
+            {
+                text: 'ripassare html',
+                done: false
+            },
+            {
+                text: 'ripassare css',
+                done: false
+            }
+        ]
+    },
+    methods : {
+        addNewToDo () {
+
+        }
+    }
+})
